ad6f862fe76ecd8aaae6dfc74259adaea40ccfbd
siotouto <siotouto@live.com>
2016-11-29 15:27:55 +0900

    mockTimeを用いたテストを強化

--- src/test/scala/interndiary/service/DiaryAppSpec.scala
+++ src/test/scala/interndiary/service/DiaryAppSpec.scala
@@ -97,6 +97,50 @@ class DiaryAppSpec extends UnitSpec with SetupDB {
         }
       )
     }
+
+    it("should be able to delete2") {
+      val app = createApp()
+      val entry3 = mockTime(1430000000000L){
+        app.write("4th entry","tired.").right.get
+      }
+      val entry1 = mockTime(1410000000000L){
+        app.write("2nd entry","I won't see bugs anymore.").right.get
+      }
+      val entry0 = mockTime(1400000000000L){
+        app.write("3rd entry","I will delete all bugs.").right.get
+      }
+      val entry2 = mockTime(1420000000000L){
+        app.write("Test title","This is unit test.").right.get
+      }
+      print(app.read(app.currentUser.name))
+      app.read(app.currentUser.name).fold(
+        { _ => fail() },
+        { entries =>
+          entries.length shouldBe 4
+          entries(0) shouldBe entry3
+          entries(1) shouldBe entry2
+          entries(2) shouldBe entry1
+        }
+      )
+      app.delete(entry1.id)
+      app.read(app.currentUser.name).fold(
+        { _ => fail() },
+        { entries =>
+          entries.length shouldBe 3
+          entries(0) shouldBe entry3
+          entries(2) shouldBe entry0
+        }
+      )
+      app.delete(entry3.id)
+      app.read(app.currentUser.name).fold(
+        { _ => fail() },
+        { entries =>
+          entries.length shouldBe 2
+          entries(0) shouldBe entry2
+          entries(1) shouldBe entry0
+        }
+      )
+    }
     // comment has been not supported yet.
   }
 }
