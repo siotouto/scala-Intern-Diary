d372425d1096ba01492d041b27a4b9a35e2538fe
siotouto <siotouto@live.com>
2016-12-16 14:48:42 +0900

    まだ途中ですが投げます。

--- src/main/scala/interndiary/web/DiaryWeb.scala
+++ src/main/scala/interndiary/web/DiaryWeb.scala
@@ -3,6 +3,16 @@ package interndiary.web
 import interndiary.service.{Context, DiaryApp}
 import org.scalatra._
 import scala.util.control.Exception.allCatch
+import interndiary.service.{
+  FailedToEditEntryError,
+  FailedToListDiaryError,
+  FailedToDeleteError,
+  FailedToGetCommentsError,
+  UserNotFoundError,
+  EntryNotFoundError,
+  UnauthorizedError
+}
+
 
 class DiaryWeb extends DiaryWebStack {
 
@@ -32,14 +42,19 @@ class DiaryWeb extends DiaryWebStack {
     val app = createApp()
 
     (for {
-      offset <- toIntEither(params.get("offset").getOrElse("0"))(BadRequest()).right
-      userName <- getEither("userName")(BadRequest()).right
+      offset <- toIntEither(
+        params.get("offset").getOrElse("0"))(
+        BadRequest("Invalid Request.")
+      ).right
+      userName <- getEither("userName")(BadRequest("Invalid Request.")).right
       entries <- app.read(userName, offset).right
     } yield (userName, entries)) match {
       case Right((userName, component)) => {
         val authorized: Boolean = app.isAuthorName(userName)
         interndiary.html.read(userName, component, authorized)
       }
+      case Left(UserNotFoundError) => NotFound(UserNotFoundError.toString)
+      case Left(FailedToListDiaryError) => InternalServerError(FailedToListDiaryError.toString)
       case Left(error) => error
     }
   }
@@ -52,15 +67,16 @@ class DiaryWeb extends DiaryWebStack {
     val app = createApp()
 
     (for {
-      rawTitle <- getEither("title")(BadRequest()).right
+      rawTitle <- getEither("title")(BadRequest("Can't get Title.")).right
       title <- (rawTitle.trim match {
-        case "" => Left(EmptyTitleEntryError)
+        case "" => Left(BadRequest("Empty title is invalid."))
         case _ => Right(rawTitle)
       }).right
-      body <- getEither("body")(BadRequest()).right
+      body <- getEither("body")(BadRequest("Can't get Body")).right
       entry <- app.write(title, body).right
     } yield entry) match {
-      case Right(entry) => Found("/")
+      case Right(entry) => Found(s"/user/${currentUserName}/entry/${entry.id}")
+      case Left(FailedToEditEntryError) => InternalServerError(FailedToEditEntryError.toString)
       case Left(error) => error
     }
   }
@@ -69,15 +85,18 @@ class DiaryWeb extends DiaryWebStack {
     val app = createApp()
 
     (for {
-      userName <- getEither("userName")(BadRequest()).right
-      rawEntryId <- getEither("entryId")(BadRequest()).right
-      entryId <- toLongEither(rawEntryId)(BadRequest()).right
+      userName <- getEither("userName")(BadRequest("Invalid request.")).right
+      rawEntryId <- getEither("entryId")(BadRequest("Invalid request.")).right
+      entryId <- toLongEither(rawEntryId)(BadRequest("Entry id must be Integer.")).right
       commentedEntry <- app.find(userName, entryId).right
     } yield (userName, commentedEntry)) match {
       case Right((userName, (entry, comments))) => {
         val authorized: Boolean = app.isAuthorName(userName)
         interndiary.html.find(entry, comments, authorized)
       }
+      case Left(UserNotFoundError) => NotFound(UserNotFoundError.toString)
+      case Left(EntryNotFoundError) => NotFound(EntryNotFoundError.toString)
+      case Left(FailedToGetCommentsError) => InternalServerError(FailedToGetCommentsError.toString)
       case Left(error) => error
     }
   }
@@ -86,12 +105,16 @@ class DiaryWeb extends DiaryWebStack {
     val app = createApp()
 
     (for {
-      userName <- getEither("userName")(BadRequest()).right
-      rawEntryId <- getEither("entryId")(BadRequest()).right
-      entryId <- toLongEither(rawEntryId)(BadRequest()).right
+      userName <- getEither("userName")(BadRequest("Invalid request.")).right
+      rawEntryId <- getEither("entryId")(BadRequest("Invalid request.")).right
+      entryId <- toLongEither(rawEntryId)(BadRequest("Entry id must be Integer.")).right
       commentedEntry <- app.find(userName, entryId).right
+      _ <- Some(userName).filter(app.isAuthorName).toRight(UnauthorizedError).right
     } yield commentedEntry) match {
       case Right((entry, comments)) => interndiary.html.edit(entry)
+      case Left(UserNotFoundError) => NotFound(UserNotFoundError.toString)
+      case Left(EntryNotFoundError) => NotFound(EntryNotFoundError.toString)
+      case Left(UnauthorizedError) => Forbidden(UnauthorizedError.toString)
       case Left(error) => error
     }
   }
@@ -100,19 +123,23 @@ class DiaryWeb extends DiaryWebStack {
     val app = createApp()
 
     (for {
-      userName <- getEither("userName")(BadRequest()).right
-      rawEntryId <- getEither("entryId")(BadRequest()).right
-      entryId <- toLongEither(rawEntryId)(BadRequest()).right
+      userName <- getEither("userName")(BadRequest("Invalid request.")).right
+      rawEntryId <- getEither("entryId")(BadRequest("Invalid request.")).right
+      entryId <- toLongEither(rawEntryId)(BadRequest("Entry id must be Integer.")).right
       _ <- app.find(userName, entryId).right
-      rawTitle <- getEither("title")(BadRequest()).right
+      rawTitle <- getEither("title")(BadRequest("Invalid request.")).right
       title <- (rawTitle.trim match {
-        case "" => Left(EmptyTitleEntryError)
+        case "" => Left(BadRequest("Empty title is invalid."))
         case _ => Right(rawTitle)
       }).right
       body <- getEither("body")(BadRequest()).right
       newEntry <- app.update(userName, entryId, title, body).right
     } yield newEntry) match {
-      case Right(entry) => Found("./")
+      case Right(entry) => Found(s"/user/${currentUserName}/entry/${entry.id}")
+      case Left(UserNotFoundError) => NotFound(UserNotFoundError.toString)
+      case Left(EntryNotFoundError) => NotFound(EntryNotFoundError.toString)
+      case Left(UnauthorizedError) => Forbidden(UnauthorizedError.toString)
+      case Left(FailedToEditEntryError) => InternalServerError(FailedToEditEntryError.toString)
       case Left(error) => error
     }
   }
@@ -121,12 +148,16 @@ class DiaryWeb extends DiaryWebStack {
     val app = createApp()
 
     (for {
-      userName <- getEither("userName")(BadRequest()).right
-      rawEntryId <- getEither("entryId")(BadRequest()).right
-      entryId <- toLongEither(rawEntryId)(BadRequest()).right
+      userName <- getEither("userName")(BadRequest("Invalid request.")).right
+      rawEntryId <- getEither("entryId")(BadRequest("Invalid request.")).right
+      entryId <- toLongEither(rawEntryId)(BadRequest("Entry id must be Integer.")).right
       commentedEntry <- app.find(userName, entryId).right
+      _ <- Some(userName).filter(app.isAuthorName).toRight(UnauthorizedError).right
     } yield commentedEntry) match {
       case Right((entry, comments)) => interndiary.html.delete(entry)
+      case Left(UserNotFoundError) => NotFound(UserNotFoundError.toString)
+      case Left(EntryNotFoundError) => NotFound(EntryNotFoundError.toString)
+      case Left(UnauthorizedError) => Forbidden(UnauthorizedError.toString)
       case Left(error) => error
     }
   }
@@ -141,6 +172,9 @@ class DiaryWeb extends DiaryWebStack {
       commentedEntry <- app.delete(userName, entryId).right
     } yield commentedEntry) match {
       case Right(entry) => Found("/")
+      case Left(UserNotFoundError) => NotFound(UserNotFoundError.toString)
+      case Left(EntryNotFoundError) => Found("/")
+      case Left(UnauthorizedError) => Forbidden(UnauthorizedError.toString)
       case Left(error) => error
     }
   }
